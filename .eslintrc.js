014f5bfc7f53ba570348132ab243e115ae543ba6
daahyunk <ekgusdl0408@naver.com>
2025-03-29 02:32:30 +0900

    init: 프로젝트 초기 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+      'prettier/prettier': 'warn',
+      'react/react-in-jsx-scope': 'off',
+    },
+  };
+  
