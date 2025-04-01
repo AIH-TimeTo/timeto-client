189a95bda3c70d8aa395bd36990686ca7b161162
daahyunk <ekgusdl0408@naver.com>
2025-04-01 12:12:52 +0900

    feat: 필요한 아이콘 파일 추가

---
+++ src/shared/assets/svgs/IcCommonKebab.tsx
@@ -0,0 +1,3 @@
+import type { SVGProps } from "react";
+const SvgIcCommonKebab = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}><path stroke="#000" strokeLinejoin="round" strokeWidth={3} d="M12 12v.01h-.01V12zm7 0v.01h-.01V12zM5 12v.01h-.01V12z" /></svg>;
+export default SvgIcCommonKebab;
