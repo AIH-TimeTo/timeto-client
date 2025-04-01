a31ea424f302ea0a556535d41a69617032d86bfb
daahyunk <ekgusdl0408@naver.com>
2025-04-02 01:24:11 +0900

    feat: 아이콘 추가

---
+++ src/shared/assets/svgs/IcPaletteCheck.tsx
@@ -0,0 +1,18 @@
+import type { SVGProps } from 'react';
+const SvgIcPaletteCheck = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 18 14"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M1 7.72 6.867 13 17 1"
+    />
+  </svg>
+);
+export default SvgIcPaletteCheck;
