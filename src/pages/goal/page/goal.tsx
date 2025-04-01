d67f3aa9005022397934ced95d197de181501244
daahyunk <ekgusdl0408@naver.com>
2025-04-02 01:23:59 +0900

    feat: 목표 추가 모달 플로우 구현

--- src/pages/goal/page/goal.tsx
+++ src/pages/goal/page/goal.tsx
@@ -1,12 +1,62 @@
-import { IcCommonHamburger24, IcCommonPlus } from '@shared/assets/svgs/index';
+import { IcCommonHamburger24, IcCommonPlus } from '@shared/assets/svgs';
 import Header from '@shared/components/header/header';
+import McList from '@shared/components/mc-list/mc-list';
+import { ModalContainer } from '@shared/components/modal-container/modal-container';
 
+import AddGoalFlow from '../components/add-goal-flow/add-goal-flow';
 import GoalGroup from '../components/goal-group/goal-group';
+import { GOAL_LIST_ITEMS } from '../constants/goal-list-items';
+import { useGoalModal } from '../hooks/use-goal-modal';
 import { mockGoalGroups } from '../mocks/goal-group';
 
 import * as styles from './goal.css';
 
 export default function GoalPage() {
+  const {
+    modalType,
+    goalName,
+    goalColor,
+    openModal,
+    closeModal,
+    goBack,
+    setGoalName,
+    setGoalColor,
+  } = useGoalModal();
+
+  const renderModal = () => {
+    if (modalType === 'list') {
+      return (
+        <ModalContainer onClose={closeModal}>
+          <McList
+            title="새로 추가하기"
+            items={GOAL_LIST_ITEMS}
+            onItemClick={(id) => {
+              if (id === 'goal') openModal('input-goal');
+              else if (id === 'folder') openModal('input-folder');
+            }}
+          />
+        </ModalContainer>
+      );
+    }
+
+    if (modalType === 'input-goal' || modalType === 'palette') {
+      return (
+        <AddGoalFlow
+          modalType={modalType}
+          goalName={goalName}
+          goalColor={goalColor}
+          onSetName={setGoalName}
+          onSetColor={setGoalColor}
+          onBack={goBack}
+          onNext={openModal}
+          onClose={closeModal}
+        />
+      );
+    }
+
+    return null;
+  };
+
   return (
     <>
       <Header
@@ -16,8 +66,13 @@ export default function GoalPage() {
           </div>
         }
         centerSlot={<h1 className={styles.goalPageTitle}>내 목표</h1>}
-        rightSlot={<IcCommonPlus width={24} height={24} />}
+        rightSlot={
+          <button onClick={() => openModal('list')}>
+            <IcCommonPlus width={24} height={24} />
+          </button>
+        }
       />
+
       <main>
         {mockGoalGroups.map((group) => (
           <GoalGroup
@@ -28,6 +83,8 @@ export default function GoalPage() {
           />
         ))}
       </main>
+
+      {renderModal()}
     </>
   );
 }
