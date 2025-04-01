3a1476b9cfa5e480b716396fa4495202c5529ccc
daahyunk <ekgusdl0408@naver.com>
2025-04-01 12:11:55 +0900

    setting: svgr 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin(), tsconfigPaths()],
+  plugins: [react(), vanillaExtractPlugin(), tsconfigPaths(), svgr()],
   server: {
     host: true,
   },
