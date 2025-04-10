4f4fa94431a5e31879ceadc6835c7b47418f857b
daahyunk <ekgusdl0408@naver.com>
2025-04-11 02:57:23 +0900

    feat: display 토큰 추가

--- src/shared/styles/tokens/display.ts
+++ src/shared/styles/tokens/display.ts
@@ -7,4 +7,13 @@ export const display = {
     display: 'flex',
     flexDirection: 'column',
   },
+  flexAlignJustifyCenter: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flexJustifyCenter: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
 } as const;
