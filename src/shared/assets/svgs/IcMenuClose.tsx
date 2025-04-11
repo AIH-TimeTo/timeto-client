e66e0723d0b4940c90131a7d55f19942d6722bf9
daahyunk <ekgusdl0408@naver.com>
2025-04-11 15:56:35 +0900

    feat: 필요한 아이콘 파일 추가

---
+++ src/shared/assets/svgs/IcMenuClose.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from 'react';
+const SvgIcMenuClose = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#000"
+      fillRule="evenodd"
+      d="M4.53 2.959a.75.75 0 1 0-1.06 1.06l7.468 7.468-7.24 7.377c-.28.286-.267.767.03 1.074.296.307.764.324 1.045.038l7.258-7.396 7.44 7.44a.75.75 0 1 0 1.06-1.061l-7.45-7.45 6.886-7.015c.28-.286.268-.767-.029-1.075-.296-.307-.765-.324-1.045-.038l-6.905 7.035z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgIcMenuClose;
