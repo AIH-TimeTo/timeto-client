93fd11f7719384db080cf1448b69fcd7d539480b
daahyunk <ekgusdl0408@naver.com>
2025-04-12 03:26:07 +0900

    refactor: 인증 방식 수정

--- src/shared/router/root-direct.tsx
+++ src/shared/router/root-direct.tsx
@@ -1,10 +1,23 @@
-import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 
 import { routePath } from '@shared/constants/path';
 import { useUserInfo } from '@shared/hooks/use-user-info';
 
 export default function RootRedirect() {
   const { isLoading } = useUserInfo();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const token = searchParams.get('token');
+
+    if (token) {
+      localStorage.setItem('accessToken', token);
+      navigate(routePath.GOAL, { replace: true });
+    }
+  }, [location.search, navigate]);
 
   if (isLoading) return null;
 
